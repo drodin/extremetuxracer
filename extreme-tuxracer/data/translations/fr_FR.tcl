42b36bbb60f20e8fb3a23ef507b01f8de13d5b58
botsnlinux <botsnlinux@0420edf4-82e4-42fc-9478-35b55e6d67a3>
2007-09-01 18:24:34 +0000

    Updated French translation.  Much thanks to Nicosmos for his work!
    git-svn-id: https://svn.code.sf.net/p/extremetuxracer/code/trunk@3 0420edf4-82e4-42fc-9478-35b55e6d67a3

--- extreme-tuxracer/data/translations/fr_FR.tcl
+++ extreme-tuxracer/data/translations/fr_FR.tcl
@@ -1,21 +1,20 @@
-
 #common strings
 pp_translate_string "Back" "Retour"
-pp_translate_string "PRESS ANY KEY TO START" "APPUYEZ SUR N'IMPORTE QUELLE TOUCHE POUR DEMARRER"
+pp_translate_string "PRESS ANY KEY TO START" "APPUYEZ SUR UNE TOUCHE POUR DÉMARRER"
 
 #game_type_select
-pp_translate_string "Enter an event" "Participer a un événement"
+pp_translate_string "Enter an event" "Compétition"
 pp_translate_string "Practice" "Entraînement"
-#pp_translate_string "Configuration" "Configuration"
+pp_translate_string "Configuration" "Configuration"
 pp_translate_string "Credits" "Crédits"
 pp_translate_string "Quit" "Quitter"
 
 #configuration
-pp_translate_string "Graphics" "Graphiques"
+pp_translate_string "Graphics" "Graphismes"
 pp_translate_string "Video" "Vidéo"
-#pp_translate_string "Audio" "Audio"
+pp_translate_string "Audio" "Audio"
 pp_translate_string "Keyboard" "Clavier"
-#pp_translate_string "Joystick" ""
+pp_translate_string "Joystick" "Joystick"
 
 #config mode
 pp_translate_string "Cancel" "Annuler"
@@ -26,45 +25,45 @@ pp_translate_string "Graphics Configuration" "Configuration graphique"
 pp_translate_string "Language:" "Langue :"
 pp_translate_string "Show UI Snow:" "Neige dans l'interface :"
 pp_translate_string "Display FPS:" "Afficher FPS :"
-pp_translate_string "Display Progress Bar:" "Afficher barre de progression :"
+pp_translate_string "Display Progress Bar:" "Afficher la barre de progression :"
 pp_translate_string "Draw Fog:" "Brouillard :"
-pp_translate_string "Reflections:" "Réflections :"
+pp_translate_string "Reflections:" "Reflets :"
 pp_translate_string "Shadows:" "Ombres :"
 
 #video configuration
-pp_translate_string "Video Configuration" "Configuration video"
-pp_translate_string "Resolution:" "Resolution :"
+pp_translate_string "Video Configuration" "Configuration vidéo"
+pp_translate_string "Resolution:" "Résolution :"
 pp_translate_string "Bits Per Pixel:" "Bits par pixel :"
 pp_translate_string "Fullscreen:" "Plein écran :"
 pp_translate_string "Experimental (needs restart)" "Expérimental (nécessite un redémarrage)"
 pp_translate_string "Enable FSAA:" "Activer FSAA :"
 
 #audio configuration
-pp_translate_string "Audio Configuration" "Configuration Audio"
+pp_translate_string "Audio Configuration" "Configuration audio"
 pp_translate_string "Sound Effects:" "Effets sonores :"
-#pp_translate_string "Music:" ""
+pp_translate_string "Music:" "Musique"
 pp_translate_string "(needs restart)" "(nécessite un redémarrage)"
-pp_translate_string "Disable Audio:" "Désactiver l'audio :"
+pp_translate_string "Disable Audio:" "Désactiver le son :"
 pp_translate_string "Stereo:" "Stéréo :"
 pp_translate_string "Bits Per Sample:" "Bits par échantillon :"
-pp_translate_string "Samples Per Second:" "Echantillons par Seconde :"
+pp_translate_string "Samples Per Second:" "Échantillons par seconde :"
 
 #keyboard configuration
 pp_translate_string "Keyboard Configuration" "Configuration du clavier"
 pp_translate_string "Turn left:" "Tourner à gauche :"
 pp_translate_string "Turn right:" "Tourner à droite :"
-pp_translate_string "Paddle:" "Palette :"
+pp_translate_string "Paddle:" "Accélérer :"
 pp_translate_string "Brake:" "Freiner :"
 pp_translate_string "Jump:" "Sauter :"
-pp_translate_string "Trick:" "Tour :"
+pp_translate_string "Trick:" "Saut périlleux :"
 pp_translate_string "Reset:" "Réinitialiser :"
 
 #joystick configuration
 pp_translate_string "Joystick Configuration" "Configuration du Joystick"
-pp_translate_string "Enable Joystick" "Activer le Joystick"
+pp_translate_string "Enable Joystick" "Utiliser le Joystick"
 
 #race select
-pp_translate_string "Race!" "Partez !"
+pp_translate_string "Race!" "C'est parti !"
 pp_translate_string "Select a race" "Choisissez une course"
 pp_translate_string "Contributed by:" "Auteur :"
 pp_translate_string "Unknown" "Inconnu"
@@ -74,39 +73,39 @@ pp_translate_string "Score:" "Score :"
 
 #event select
 pp_translate_string "Continue" "Continuer"
-pp_translate_string "Select event and cup" "Choisissez événement et coupe"
-pp_translate_string "Event:" "Evénement :"
+pp_translate_string "Select event and cup" "Choisissez une compétition et une coupe"
+pp_translate_string "Event:" "Compétition :"
 pp_translate_string "Cup:" "Coupe :"
-pp_translate_string "You've won this cup!" "Vous avez gagne cette coupe !"
-pp_translate_string "You must complete this cup next" "Vous devez gagner cette coupe maintenant"
-pp_translate_string "You cannot enter this cup yet" "Vous ne pouvez pas encore participer a cette coupe"
+pp_translate_string "You've won this cup!" "Vous avez remporté cette coupe !"
+pp_translate_string "You must complete this cup next" "Vous devez remporter cette coupe"
+pp_translate_string "You cannot enter this cup yet" "Vous ne pouvez pas encore participer à cette coupe"
 
 #event race select
-pp_translate_string "You don't have any lives left." "Il ne vous reste plus de vies"
-pp_translate_string "Race won! Your result:" "Course gagnée ! Votre score :"
+pp_translate_string "You don't have any lives left." "Il ne vous reste plus aucune vie."
+pp_translate_string "Race won! Your result:" "Vous avez remporté la coupe ! Votre score :"
 pp_translate_string "Needed to advance:" "Nécessaire pour avancer :"
-pp_translate_string "You can't enter this race yet." "Vous ne pouvez pas participer à cette course maintenant"
+pp_translate_string "You can't enter this race yet." "Vous ne pouvez pas encore participer à cette course."
 
 #loading
-pp_translate_string "Loading, Please Wait..." "Chargement, veuillez patienter..."
+pp_translate_string "Loading, Please Wait..." "Chargement, veuillez patienter ..."
 
 #paused
 pp_translate_string "Resume" "Reprendre"
 pp_translate_string "Paused" "Pause"
 
 #race over
-pp_translate_string "Race Over" "Course terminee"
+pp_translate_string "Race Over" "Course terminée"
 pp_translate_string "Time: %02d:%02d.%02d" "Temps : %02d:%02d.%02d"
 pp_translate_string "Herring: %3d" "Harengs : %3d"
 pp_translate_string "Score: %6d" "Score : %6d"
 pp_translate_string "Max speed: %3d km/h" "Vitesse maximale : %3d km/h"
-pp_translate_string "Was flying: %.01f %% of time" "A volé : %.01f %% du temps"
+pp_translate_string "Was flying: %.01f %% of time" "A volé %.01f %% du temps"
 pp_translate_string "Race aborted" "Course annulée"
-pp_translate_string "You beat your best score!" "Vous avez battu votre score"
-pp_translate_string "Congratulations! You won the event!" "Félicitations, vous avez gagné l'événement !"
+pp_translate_string "You beat your best score!" "Vous avez battu votre score !"
+pp_translate_string "Congratulations! You won the event!" "Félicitations, vous avez gagné la compétition !"
 pp_translate_string "Congratulations! You won the cup!" "Félicitations, vous avez gagné la coupe !"
-pp_translate_string "You advanced to the next race!" "Vous avez avancé jusqu'à la course suivante !"
-pp_translate_string "You didn't advance." "Vous n'avez pas avancé"
+pp_translate_string "You advanced to the next race!" "Vous avancez jusqu'à la course suivante !"
+pp_translate_string "You didn't advance." "Vous n'avez pas avancé."
 
 #HUD
 #Strings are prefixed to prevent collisions with the UI
