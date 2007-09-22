d1d1cffcd324fad88c87f13b5466d1fb55f53387
cpicon92 <cpicon92@0420edf4-82e4-42fc-9478-35b55e6d67a3>
2007-09-22 02:48:35 +0000

    added new courses
    git-svn-id: https://svn.code.sf.net/p/extremetuxracer/code/trunk@23 0420edf4-82e4-42fc-9478-35b55e6d67a3

---
+++ extreme-tuxracer/data/courses/events/mountain_mania/explore_mountains/course.tcl
@@ -0,0 +1,10 @@
+tux_course_name "Explore the Mountains"
+tux_course_author "Reinhard Niehoff <rei.nie@erinacom.de>"
+tux_course_dim 100 2000 90 1980
+tux_start_pt 50 3.0
+tux_angle 25
+tux_elev_scale 12
+tux_base_height_value 255
+tux_elev elev.png
+tux_terrain terrain.png
+tux_course_init
