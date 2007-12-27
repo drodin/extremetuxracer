39085bbf2ab8ad4e778fa0775e3ee74b983f91e6
cpicon92 <cpicon92@0420edf4-82e4-42fc-9478-35b55e6d67a3>
2007-12-27 21:18:34 +0000

    added polish translation
    git-svn-id: https://svn.code.sf.net/p/extremetuxracer/code/trunk@49 0420edf4-82e4-42fc-9478-35b55e6d67a3

---
+++ extreme-tuxracer/data/translations/pl_PL.tcl
@@ -0,0 +1,145 @@
+#Polish translation maintained by woody
+#In case of any problems or suggestions, please feel free to contact me at czajernia@gmail.com
+
+#common strings
+pp_translate_string "Back" "Wstecz"
+pp_translate_string "PRESS ANY KEY TO START" "NACISNIJ DOWOLNY KLAWISZ"
+
+#game_type_select
+pp_translate_string "Enter an event" "Rozpocznij gre"
+pp_translate_string "Practice" "Cwiczenia"
+pp_translate_string "Configuration" "Ustawienia"
+pp_translate_string "Credits" "O autorach"
+pp_translate_string "Quit" "Wyjscie"
+
+#configuration
+pp_translate_string "Graphics" "Grafika"
+pp_translate_string "Video" "Ekran"
+pp_translate_string "Audio" "Dzwiek"
+pp_translate_string "Keyboard" "Klawiatura"
+pp_translate_string "Joystick" "Joystick"
+
+#config mode
+pp_translate_string "Cancel" "Anuluj"
+pp_translate_string "Ok" "OK"
+
+#graphics configuration
+pp_translate_string "Graphics Configuration" "Ustawienia grafiki"
+pp_translate_string "Language:" "Jezyk:"
+pp_translate_string "Show UI Snow:" "Snieg w menu:"
+pp_translate_string "Display FPS:" "Licznik FPS:"
+pp_translate_string "Display Progress Bar:" "Pasek postepu:"
+pp_translate_string "Draw Fog:" "Mgla:"
+pp_translate_string "Reflections:" "Refleksy:"
+pp_translate_string "Shadows:" "Cienie:"
+pp_translate_string "Model:" "Postac:"
+pp_translate_string "Tux on Snowboard" "Tux na Snowboardzie"
+
+#video configuration
+pp_translate_string "Video Configuration" "Ustawienia ekranu"
+pp_translate_string "Resolution:" "Rozdzielczosc:"
+pp_translate_string "Bits Per Pixel:" "Glebia koloru:"
+pp_translate_string "Fullscreen:" "Pelny ekran:"
+pp_translate_string "Experimental (needs restart)" "Ust. eksperymentalne (wymagaja restartu)"
+pp_translate_string "Enable FSAA:" "Wlacz FSAA:"
+pp_translate_string "To change the resolution, or switch into fullscreen mode" "Aby zmienic rozdzielczosc lub przelaczyc na pelny ekran"
+pp_translate_string "use options.txt, located in the config folder." "uzyj pliku options.txt, który znajduje sie w folderze config."
+
+#audio configuration
+pp_translate_string "Audio Configuration" "Ustawienia dzwieku"
+pp_translate_string "Sound Effects:" "Efekty dzwiekowe:"
+pp_translate_string "Music:" "Muzyka:"
+pp_translate_string "(needs restart)" "(wymagaja restartu)"
+pp_translate_string "Disable Audio:" "Wylacz dzwiek:"
+pp_translate_string "Stereo:" "Stereo:"
+pp_translate_string "Bits Per Sample:" "Czestotliwosc próbkowania:"
+pp_translate_string "Samples Per Second:" "Próbek na sekunde:"
+
+#keyboard configuration
+pp_translate_string "Keyboard Configuration" "Ustawienia klawiatury"
+pp_translate_string "Turn left:" "Skret w lewo:"
+pp_translate_string "Turn right:" "Skret w prawo:"
+pp_translate_string "Paddle:" "Rozpedzanie:"
+pp_translate_string "Brake:" "Hamowanie:"
+pp_translate_string "Jump:" "Skok:"
+pp_translate_string "Trick:" "Trik:"
+pp_translate_string "Reset:" "Reset:"
+
+#joystick configuration
+pp_translate_string "Joystick Configuration" "Ustawienia Joysticka"
+pp_translate_string "Enable Joystick" "Wlacz Joystick"
+
+#race select
+pp_translate_string "Race!" "Start!"
+pp_translate_string "Select a race" "Wybierz trase"
+pp_translate_string "Contributed by:" "Stworzona przez:"
+pp_translate_string "Unknown" "autor nieznany"
+pp_translate_string "Time:" "Czas:"
+pp_translate_string "Herring:" "Sledzie:"
+pp_translate_string "Score:" "Wynik:"
+
+#event select
+pp_translate_string "Continue" "Kontynuuj"
+pp_translate_string "Select event and cup" "Wybierz rozgrywki i puchar"
+pp_translate_string "Event:" "Rozgrywki:"
+pp_translate_string "Cup:" "Puchar:"
+pp_translate_string "You've won this cup!" "Wygrales ten puchar!"
+pp_translate_string "You must complete this cup next" "Musisz teraz ukonczyc ten puchar"
+pp_translate_string "You cannot enter this cup yet" "Jeszcze nie mozesz wystartowac w tym pucharze"
+
+#event race select
+pp_translate_string "You don't have any lives left." "Nie masz juz mozliwych szansz."
+pp_translate_string "Race won! Your result:" "Wygrales wyscig! Wyniki:"
+pp_translate_string "Needed to advance:" "Awans od:"
+pp_translate_string "You can't enter this race yet." "Jeszcze nie mozesz startowac na tej trasie."
+
+#loading
+pp_translate_string "Loading, Please Wait..." "Ladowanie, Prosze Czekac..."
+
+#paused
+pp_translate_string "Resume" "Wznów"
+pp_translate_string "Paused" "Pauza"
+
+#race over
+pp_translate_string "Race Over" "Koniec wyscigu"
+pp_translate_string "Time: %02d:%02d.%02d" "Czas: %02d:%02d.%02d"
+pp_translate_string "Herring: %3d" "Sledzie: %3d"
+pp_translate_string "Score: %6d" "Wynik: %6d"
+pp_translate_string "Max speed: %3d km/h" "Maks. szybkosc: %3d km/h"
+pp_translate_string "Was flying: %.01f %% of time" "W powietrzu: %.01f %% czasu"
+pp_translate_string "Race aborted" "Wyscig przerwany"
+pp_translate_string "You beat your best score!" "Pobiles swój rekord!"
+pp_translate_string "Congratulations! You won the event!" "Gratulacje! Wygrales kampanie!"
+pp_translate_string "Congratulations! You won the cup!" "Gratulacje! Wygrales puchar!"
+pp_translate_string "You advanced to the next race!" "Awansowales do nastepnego wyscigu!"
+pp_translate_string "You didn't advance." "Niestety, nie przeszedles dalej."
+
+#highscore
+pp_translate_string "Highscore" "Rekordy"
+pp_translate_string "You made it to the %s place in the highscore!" "Zdobyles %s miejsce na liscie rekordów!"
+pp_translate_string "Player name:" "Imie:"
+pp_translate_string "1:st" "1."
+pp_translate_string "2:nd" "2."
+pp_translate_string "3:rd" "3."
+pp_translate_string "4:th" "4."
+pp_translate_string "5:th" "5."
+pp_translate_string "6:th" "6."
+pp_translate_string "7:th" "7."
+pp_translate_string "8:th" "8."
+pp_translate_string "9:th" "9."
+pp_translate_string "10:th" "10."
+pp_translate_string " (%d points)" " (%d pkt)"
+pp_translate_string "No records" "Brak rekordów"
+
+
+#HUD
+#Strings are prefixed to prevent collisions with the UI
+pp_translate_string "H|FPS: %.1f" "FPS: %.1f"
+pp_translate_string "H|Time:" "Czas:"
+pp_translate_string "H|km/h" "km/h"
+#herring counter
+pp_translate_string "H|%03d" "%03d"
+#time counter
+pp_translate_string "H|%02d:%02d:%02d" "%02d:%02d:%02d"
+#speed counter
+pp_translate_string "H|%.0f" "%.0f" 
