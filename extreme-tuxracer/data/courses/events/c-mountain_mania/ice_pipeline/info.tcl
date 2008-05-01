67fbfb8eb1135887f996998dd514d0922e7cd5f2
cpicon92 <cpicon92@0420edf4-82e4-42fc-9478-35b55e6d67a3>
2008-05-01 18:49:14 +0000

    fixed ice pipeline requirements
    git-svn-id: https://svn.code.sf.net/p/extremetuxracer/code/trunk@109 0420edf4-82e4-42fc-9478-35b55e6d67a3

--- extreme-tuxracer/data/courses/events/c-mountain_mania/ice_pipeline/info.tcl
+++ extreme-tuxracer/data/courses/events/c-mountain_mania/ice_pipeline/info.tcl
@@ -2,8 +2,8 @@ set course_name "Ice Pipeline"
 set course_author "John Michael Mulesa <slipstream2001@mac.com>"
 set course_description "A short, slippery ice-trough with herring scattered throughout."
 set par_time 80.0
-set herring { 60 60 60 60 }
-set times { 0 15 0 0 }
+set herring { 30 30 30 30 }
+set times { 0 120 0 0 }
 set score { 0 0 0 0 }
 set mirrored no
 set conditions sunny
